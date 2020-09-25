d58f105f5f53740990bc43c1ec62e07bf5e44d73
Arpit <arpit.agrawal@mavs.uta.edu>
2020-09-24 20:22:53 -0500

    updated file to add html page

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -4,9 +4,3 @@ from django.http import HttpResponse
 
 def home(request):
 	return render(request, 'firstapp/home.html')
-
-# def about(request):
-# 	return HttpResponse("About page")
-
-# def contact(request):
-# 	return HttpResponse("Contact us")
