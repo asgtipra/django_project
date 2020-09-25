6ecf08dc5ca08faafbbe33dfb11aef64ad566cf3
Arpit <arpit.agrawal@mavs.uta.edu>
2020-09-24 19:58:17 -0500

    created personalized views.py for myapp

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+	return render(request, 'firstapp/home.html')
+
+# def about(request):
+# 	return HttpResponse("About page")
+
+# def contact(request):
+# 	return HttpResponse("Contact us")
