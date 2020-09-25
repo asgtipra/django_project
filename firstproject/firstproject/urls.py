237cc4015cddc296340603aa681017729e5fcc53
Arpit <arpit.agrawal@mavs.uta.edu>
2020-09-24 20:14:10 -0500

    mapped views.py file

--- firstproject/firstproject/urls.py
+++ firstproject/firstproject/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from firstapp import views			#-> importing home function from views.py
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('firstapp.urls')),
 ]
