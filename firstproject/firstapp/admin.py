2982aa3dab89ef64fc2c8ba2f6f401aa0053f74b
Arpit <arpit.agrawal@mavs.uta.edu>
2020-09-28 16:13:29 -0500

    registered models

--- firstproject/firstapp/admin.py
+++ firstproject/firstapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from firstapp.models import Login, Python, Student, Reporter, Publication, Article
 
 # Register your models here.
+admin.site.register(Login)
+admin.site.register(Python)
+admin.site.register(Student)
+admin.site.register(Reporter)
+admin.site.register(Publication)
+admin.site.register(Article)
