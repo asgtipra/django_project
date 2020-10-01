5cb0ab4b16cfe05185fcf4289af3195541f810d2
Arpit <arpit.agrawal@mavs.uta.edu>
2020-10-01 00:02:36 -0500

    included path to register.html

--- firstproject/firstapp/urls.py
+++ firstproject/firstapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from firstapp import views			#-> importing home function from views.py
+from firstapp import views			#-> importing functions from views.py
 
 urlpatterns = [
-    path('home', views.home),
+    path('home/', views.home),
+    path('register/', views.form_view, name='register')
     # path('about', views.about),
     # path('contact-us', views.contact),
 ]
