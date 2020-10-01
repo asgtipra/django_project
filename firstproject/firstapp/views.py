37799b219c9a5bf18330f384d2b56d0aaea1db59
Arpit <arpit.agrawal@mavs.uta.edu>
2020-10-01 00:03:57 -0500

    created view function for forms.py

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from firstapp import forms
 
 def home(request):
 	return render(request, 'firstapp/home.html')
+
+def form_view(request):
+	if request.method == 'POST':
+		form=forms.Register(request.POST)
+		if form.is_valid():
+			print('Success!!')
+			print("Name: " + form.cleaned_data['name'])
+			print("Email: " + form.cleaned_data['email'])
+			print("Text: " + form.cleaned_data['text'])
+		else:
+			print('Please put valid values')
+
+	form = forms.Register
+	return render(request, 'firstapp/register.html', {'form':form})
