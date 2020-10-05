573d2bf1eef4f290b8e7dab39cce7e7d23ca773e
Arpit <arpit.agrawal@mavs.uta.edu>
2020-10-05 10:02:11 -0500

    created new views

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,20 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from firstapp import forms
 
 def home(request):
 	return render(request, 'firstapp/home.html')
 
-def form_view(request):
+def create(request):
 	if request.method == 'POST':
-		form=forms.Register(request.POST)
+		form = forms.Registerform(request.POST)
 		if form.is_valid():
-			print('Success!!')
-			print("Name: " + form.cleaned_data['name'])
-			print("Email: " + form.cleaned_data['email'])
-			print("Text: " + form.cleaned_data['text'])
-		else:
-			print('Please put valid values')
+			try:
+				form.save()
+				return redirect('success')
+			except:
+				print('error')
+	else:
+		form=forms.Registerform()
+	return render (request, 'firstapp/register.html', {'form':form})
 
-	form = forms.Register
-	return render(request, 'firstapp/register.html', {'form':form})
+def success(request):
+	return render (request, 'firstapp/success.html')
+
+
+#----------------------------------------------------------------------------
+#->	this below code will display data on terminal. 
+
+# def form_view(request):
+# 	if request.method == 'POST':
+# 		form=forms.Register(request.POST)
+# 		if form.is_valid():
+# 			print('Success!!')
+# 			print("Name: " + form.cleaned_data['name'])
+# 			print("Email: " + form.cleaned_data['email'])
+# 			print("Text: " + form.cleaned_data['text'])
+# 		else:
+# 			print('Please put valid values')
+
+# 	form = forms.Registerform
+# 	return render(request, 'firstapp/register.html', {'form':form})
