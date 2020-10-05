2eae9380cc871601e4379034518a3fb48c50e464
Arpit <arpit.agrawal@mavs.uta.edu>
2020-10-05 10:00:45 -0500

    added success.html path

--- firstproject/firstapp/urls.py
+++ firstproject/firstapp/urls.py
@@ -3,7 +3,9 @@ from firstapp import views			#-> importing functions from views.py
 
 urlpatterns = [
     path('home/', views.home),
-    path('register/', views.form_view, name='register')
+    path('register/', views.create, name='register'),
+    path('success/', views.success, name='success'),
+    # path('register/', views.form_view, name='register')
     # path('about', views.about),
     # path('contact-us', views.contact),
 ]
